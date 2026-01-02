094d8bd5f3e0dcf8922a9911c46f43ac5ce1ad67
Alasdair Smith <alasdair.smith@envanceuk.com>
2026-01-02 15:11:56 +0000

    Write

---
+++ main.py
@@ -0,0 +1,11 @@
+def get_book_text(fpath):
+    with open(fpath) as f:
+        file_contents = f.read()
+    
+    return file_contents
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+if __name__ == "__main__":
+    main()
